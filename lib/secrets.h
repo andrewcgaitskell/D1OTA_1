48c34e619175c6748f5894205b2c1c63b33da536
andrewcgaitskell <andrew@gaitskell.com>
2019-12-12 11:36:29 +0000

    Update secrets.h

--- lib/secrets.h
+++ lib/secrets.h
@@ -1,7 +1,8 @@
-#ifndef STASSID
-#define STASSID "your-ssid"
-#define STAPSK  "your-password"
-#endif
+// Use this file to store all of the private credentials 
+// and connection details
 
-const char* ssid = STASSID;
-const char* password = STAPSK;
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
